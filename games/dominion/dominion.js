0c5f78cefafeb41d30eae02650ec5d771ea554f0
Zack Swafford <zdfs1121@gmail.com>
2020-05-06 22:11:12 -0700

    Beef up dominion page

--- games/dominion/dominion.js
+++ games/dominion/dominion.js
@@ -1,19 +1,64 @@
 const randomize = (() => {
     const setAmounts = {
-        'Base': 5,
-        'Empires': 5
+        'Base': 10
     };
 
-    const cardElements = {};
 
-    function randomize()
-        let chosenCards = [];
+    const sidebar = document.getElementById('sidebar');
+    const text = document.getElementById('text');
+    const images = document.getElementById('images');
+
+    Object.keys(allCards).forEach(function(set) {
+        const container = document.createElement('div');
+        container.setAttribute('class', 'foof');
+        const title = document.createElement('h2');
+        title.setAttribute('class', 'set-picker-title');
+        title.appendChild(document.createTextNode(set));
+        container.appendChild(title);
+        const picker = document.createElement('select');
+        picker.setAttribute('class', 'set-picker-counter');
+        for (let i = 1; i <= 10; i++){
+            const option = document.createElement("option");
+            option.text = i;
+            option.value = i;
+            picker.appendChild(option)
+        }
+        container.appendChild(picker);
+        sidebar.appendChild(container);
+    });
+
+
+    function randomize() {
+        text.innerHTML = '';
+        images.innerHTML = '';
+
         Object.keys(setAmounts).forEach(function(set) {
+            const title = document.createElement('h2');
+            title.setAttribute('class', 'foof');
+            title.appendChild(document.createTextNode(set));
+            text.appendChild(title);
+
             const amount = setAmounts[set];
             const setCards = allCards[set].cards;
-            chosenCards.push(...selectWithoutReplacement(setCards, amount));
+            const chosenCards = selectWithoutReplacement(setCards, amount);
+            chosenCards.sort((a, b) => a.name.localeCompare(b.name));
+
+            const list = document.createElement('ul');
+            list.setAttribute('class', 'foof');
+            chosenCards.forEach(function(card) {
+                const item = document.createElement('li');
+                item.setAttribute('class', 'foof');
+                item.appendChild(document.createTextNode(card.name));
+                text.appendChild(item);
+
+                const image = document.createElement('img');
+                image.setAttribute('class', 'img-card');
+                image.setAttribute('src', card.imgLink);
+                image.setAttribute('alt', card.name);
+                images.appendChild(image);
+            });
+            text.appendChild(list);
         });
-        console.log(chosenCards);
     }
     randomize();
     return randomize;
